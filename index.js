58778e1ad36a3d44335849354f350a48a14dbcb2
Tasadduq Ali <62251149+tasduq@users.noreply.github.com>
2021-07-22 20:54:21 +0500

    fixing

--- index.js
+++ index.js
@@ -7,14 +7,18 @@
 const express = require("express");
 const socketIO = require("socket.io");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 const INDEX = "./index.html";
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 let users = [];
 
